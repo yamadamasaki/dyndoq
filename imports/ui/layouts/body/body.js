bd2a55fda1b2501323c1f1dba9e68d3b3ea619ba
山田正樹 <masaki@metabolics.co.jp>
2017-02-02 19:41:46 +0900

    App_body レイアウトに userId を表示させてみた

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,1 +1,8 @@
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
 import './body.html';
+
+Template.App_body.helpers({
+    userId: () => Meteor.userId(),
+});
