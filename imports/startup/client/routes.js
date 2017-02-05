cdcec34cb2c8a798b1e24dd28e51747cb946ab21
山田正樹 <masaki@metabolics.co.jp>
2017-02-05 13:34:25 +0900

    customers ページを用意する

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,17 +5,27 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/customers/customers.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
-  name: 'App.home',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
-  },
+    name: 'App.home',
+    action() {
+        BlazeLayout.render('App_body', { main: 'App_home' });
+    },
 });
 
 FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
+    action() {
+        BlazeLayout.render('App_body', { main: 'App_notFound' });
+    },
 };
+
+FlowRouter.route('/customers', {
+    // 顧客データベース
+    // query params: year, group
+    name: 'customers',
+    action() {
+        BlazeLayout.render('App_body', { main: 'customers' });
+    },
+});
