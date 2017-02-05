b626c382006bb593dab1b3f287a9ce5403ead590
山田正樹 <masaki@metabolics.co.jp>
2017-02-05 16:23:54 +0900

    customers の fixtures を生成する

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
 import links from './fixtures-links.js';
+import customers from './fixtures-customers.js';
 
 Meteor.startup(() => {
     links();
+    customers();
 });
