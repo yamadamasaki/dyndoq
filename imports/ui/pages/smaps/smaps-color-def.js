a3445a08a21ec68b134d478d17baea979592fb9c
山田正樹 <masaki@metabolics.co.jp>
2017-02-22 12:40:29 +0900

    colorpaletteを表示した

--- imports/ui/pages/smaps/smaps-color-def.js
+++ imports/ui/pages/smaps/smaps-color-def.js
@@ -1,1 +1,25 @@
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
 import './smaps-color-def.html';
+
+Template.smapsColorDef.helpers({
+
+});
+
+Template.smapsColorDef.events({
+    'click .btn': event => {
+        const [cSmapId, pSmapId] = event.currentTarget.id.split('-');
+
+        $(`#${cSmapId}-${pSmapId}-fgpalette`).colorPalette()
+            .on('selectColor', function(e) {
+                console.log('.', e.color);
+                $(`#${cSmapId}-${pSmapId}-color i`).css('color', e.color);
+            });
+        $(`#${cSmapId}-${pSmapId}-bgpalette`).colorPalette()
+            .on('selectColor', function(e) {
+                console.log('..', e.color);
+                $(`#${cSmapId}-${pSmapId}-color i`).css('background-color', e.color);
+            });
+    },
+});
