7eb9928a45f8386bcbca85f8105c2b1c7964d9e1
山田正樹 <masaki@metabolics.co.jp>
2017-03-16 17:57:08 +0900

    $ meteor add ahref:dragula example を動かしてみる

--- imports/ui/pages/planning/planning.js
+++ imports/ui/pages/planning/planning.js
@@ -1,1 +1,13 @@
+import { Template } from 'meteor/templating';
+
 import './planning.html';
+
+Template.planning.helpers({
+    items: function() {
+        return [{ name: 'Item0', number: 0 }, { name: 'Item1', number: 1 }, { name: 'Item2', number: 2 }, { name: 'Item3', number: 3 }, { name: 'Item4', number: 4 }, { name: 'Item5', number: 5 }, ];
+    }
+});
+
+Template.planning.onRendered(function() {
+    dragula([document.getElementById('left1'), document.getElementById('right1')]);
+});
