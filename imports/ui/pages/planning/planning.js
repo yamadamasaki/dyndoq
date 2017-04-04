f011f18ea5f665fb196422b4bdcc6469ecf892d0
山田正樹 <masaki@metabolics.co.jp>
2017-04-04 15:38:44 +0900

    とりあえず planning をコミットする. (多分) レンダリングが間に合わないので, dragula全部にコンテナを設定できないようだ. console.logは残してある

--- imports/ui/pages/planning/planning.js
+++ imports/ui/pages/planning/planning.js
@@ -6,18 +6,42 @@ import { Departments } from '/imports/api/customers/departments.js'
 import { Tracker } from 'meteor/tracker'
 import { Meteor } from 'meteor/meteor'
 import { Visits } from '/imports/api/visits/visits.js'
+import { lodash } from 'meteor/erasaur:meteor-lodash'
+import { $ } from 'meteor/jquery'
 
 import './fill-visits.js'
 
 import './planning.html'
 
+const _ = lodash
+
 let groupName, memberId, year
+
+const registerDrake = (steps, customers) => {
+    if (!steps || !customers) return
+    const srcs = _.range(12).map(it => {
+        return document.getElementById(`visit-${it+1}`)
+    })
+    const dests = _.flatten(customers.map(customer => {
+        return steps.map(step => {
+            console.log("registerDrake: ", document.getElementById(`visit-${customer}-${step}`))
+            return document.getElementById(`visit-${customer}-${step}`)
+        })
+    }))
+    console.log("3P7CrqNrcYxBeG: ", document.getElementById('3P7CrqNrcYxBeG'))
+    const drake = dragula()
+    srcs.forEach(it => drake.containers.push(it))
+    dests.forEach(it => drake.containers.push(it))
+    console.log("drake: ", drake.containers)
+    return drake
+}
 
 Template.planning.onCreated(() => {
     groupName = FlowRouter.getParam('group')
     memberId = FlowRouter.getParam('member')
     year = parseInt(FlowRouter.getParam('year'))
     Tracker.autorun(() => {
+        console.log("autorun onCreated")
         const u = Meteor.user()
         if (u && groupName) {
             Meteor.subscribe('members.bygroup', u.tenant, groupName)
@@ -28,15 +52,24 @@ Template.planning.onCreated(() => {
     })
 })
 
+Template.planning.onRendered(function() {
+    Template.instance().autorun(() => {
+        console.log("autorun onRendered")
+        const drake = registerDrake(steps(), customers())
+        console.log("autorun: ", drake)
+    })
+})
+
 const memberName = () => (Meteor.users.findOne((Members.findOne(memberId) || {}).account) || {}).username
 
+const steps = () => (Object.keys(((Members.findOne(memberId) || {}).steps || {})) || {}).filter(it => it !== '受注')
+
+const customers = () => (Members.findOne(memberId) || {}).inChargeOf
+
 Template.planning.helpers({
-    items: function() {
-        return [{ name: 'Item0', number: 0 }, { name: 'Item1', number: 1 }, { name: 'Item2', number: 2 }, { name: 'Item3', number: 3 }, { name: 'Item4', number: 4 }, { name: 'Item5', number: 5 }, ]
-    },
     months: () => [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3],
-    steps: () => Object.keys((Members.findOne(memberId) || {}).steps || {}),
-    customers: () => (Members.findOne(memberId) || {}).inChargeOf,
+    steps,
+    customers,
     dname: departmentId => (Departments.findOne(departmentId) || {}).name,
     cname: departmentId => (Customers.findOne((Departments.findOne(departmentId) || {}).customer) || {}).name,
     financialYear: () => year,
@@ -45,8 +78,23 @@ Template.planning.helpers({
     memberName,
     fillVisitsArgs: () => { return { year, memberName, memberId } },
     visits: month => Visits.find({ member: memberId, financialYear: year, month, $or: [{ plannedDate: { $exists: false } }, { plannedDate: { $eq: null } }] }),
-})
-
-Template.planning.onRendered(function() {
-    dragula([document.getElementById('left1'), document.getElementById('right1')])
+    visitsGroup: month => {
+        return _.flatten(['最重要', '重要', 'その他', '新規'].map(grade => {
+            return steps().map(step => {
+                return {
+                    grade,
+                    step,
+                    n: Visits.find({
+                        member: memberId,
+                        financialYear: year,
+                        month,
+                        $or: [{ plannedDate: { $exists: false } }, { plannedDate: { $eq: null } }],
+                        customerGrade: grade,
+                        step,
+                    }).count(),
+                }
+            })
+        })).filter(it => it.n > 0)
+    },
+    visits: (customer, step) => Visits.find({ department: customer, step, member: memberId, financialYear: year, }),
 })
