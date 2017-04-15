2228ea62a956d159d7bdcf9c5a79eeca5b2faf25
山田正樹 <masaki@metabolics.co.jp>
2017-04-15 15:11:27 +0900

    営業のステップ, ゴール, 方法のセットの定義を表す Schema を作る

---
+++ imports/api/definitions/sales-steps-def.js
@@ -0,0 +1,88 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { Mongo } from 'meteor/mongo'
+
+export const SaleStepsDef = new Mongo.Collection('salesstepsdef')
+
+let Schema = {}
+
+Schema.SalesStepDefSchema = new SimpleSchema({
+    name: {
+        type: String,
+        optional: true,
+    },
+    description: {
+        type: String,
+        optional: true,
+    },
+    goals: {
+        type: [Object],
+        optional: true,
+    },
+    'goals.$.name': {
+        type: String,
+    },
+    'goals.$.description': {
+        type: String,
+        optional: true,
+    },
+    'goals.$.content': {
+        type: String,
+        optional: true,
+    },
+    methods: {
+        type: [Object],
+        optional: true,
+    },
+    'methods.$.name': {
+        type: String,
+    },
+    'methods.$.description': {
+        type: String,
+        optional: true,
+    },
+    'methods.$.content': {
+        type: String,
+        optional: true,
+    },
+})
+
+const SaleStepsDefSchema = new SimpleSchema({
+    _id: {
+        type: String,
+        optional: true,
+    },
+    _tenant: {
+        type: String,
+    },
+    _service: {
+        type: String,
+    },
+    _group: {
+        type: String,
+        defaultValue: 'default-group',
+    },
+    _owner: {
+        type: String, // userId
+        optional: true,
+    },
+    _timestamp: {
+        type: SimpleSchema.Integer, // unix time
+        defaultValue: () => new Date().getTime(),
+    },
+    _description: {
+        type: String, // 変更理由など
+        optional: true,
+    },
+    financialYear: {
+        type: SimpleSchema.Integer,
+    },
+    name: { // 定義名
+        type: String,
+    },
+    steps: { // ステップ定義
+        type: [Schema.SalesStepDefSchema],
+        optional: true
+    }
+})
+
+SaleStepsDef.attachSchema(SaleStepsDefSchema)
